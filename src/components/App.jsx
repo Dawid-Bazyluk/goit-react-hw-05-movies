5ab174e371f894a234af013fdc8b618b4972b8d2
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-06-27 23:22:11 +0200

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,9 @@
-import Profile from "./Profile";
-import Statistics from "./Statistics";
-import FriendList from "./FriendList";
-import TransactionHistory from "./TransactionHistory";
-import user from "../data/user.json";
-import data from "../data/data.json";
-import friends from "../data/friends.json";
-import transactions from "../data/transactions.json";
+
 
 function App() {
   return (
     <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
+      <h1>Bye world</h1>
     </>
   );
 }
