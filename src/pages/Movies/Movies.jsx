b04ceb06cf103c20ca30cf78a89d77f7a6e24cfb
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-06-28 14:56:55 +0200

    updt

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -9,51 +9,43 @@ import { API_KEY } from "../../functions/api";
 const Movies = () => {
   const [value, setValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [movies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${value}`;
 
   const handleSubmit = async (event) => {
-    setIsLoading(true);
     event.preventDefault();
-    event.currentTarget.reset();
+    setIsLoading(true);
+
     if (value === "") {
       setIsLoading(false);
       return Notify.info("Please provide a query");
     }
-    const movieList = await fetchMovies(
-      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${value}`,
-    );
-    if (movieList.results.length === 0) {
-      setIsLoading(false);
-      movies.length = 0;
-      return Notify.info(
-        "We could not find the movie you are looking for. Please try another title.",
-      );
-    }
-    if (movieList) {
-      setIsLoading(false);
-      const results = [...movieList.results];
-      results.map((movie) => ({
-        title: movie.title,
-        id: movie.id,
-        release_date: movie.release_date,
+
+    try {
+      const movieList = await fetchMovies(URL);
+
+      if (movieList.results.length === 0) {
+        setIsLoading(false);
+        Notify.info(
+          "We could not find the movie you are looking for. Please try another title.",
+        );
+        return;
+      }
+
+      const results = movieList.results.map((movie) => ({
+        movieId: movie.id,
+        movieTitle: movie.title,
+        releaseDate: new Date(movie.release_date).toLocaleDateString("en-US", {
+          year: "numeric",
+        }),
       }));
 
-      movies.length = 0;
-
-      results.forEach((result) => {
-        const releaseDate = new Date(result.release_date);
-        const releaseYear = releaseDate.toLocaleDateString("en-US", {
-          year: "numeric",
-        });
-
-        movies.push({
-          movieId: result.id,
-          movieTitle: result.title,
-          releaseDate: releaseYear,
-        });
-      });
-    } else {
-      throw new Error("Error in Movies page");
+      setMovies(results);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setIsLoading(false);
+      Notify.failure("Error fetching movies. Please try again later.");
     }
   };
 
